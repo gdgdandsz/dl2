7201a9b731964bce2b58aaa271f6bcc8dad037d2
Alanzxd <112223122+Alanzxd@users.noreply.github.com>
2024-04-14 11:49:34 +0800

    Update main.py

--- Model 2 - UNet/main.py
+++ Model 2 - UNet/main.py
@@ -239,7 +239,7 @@ if __name__ == "__main__":
     # loss criterion, optimizer
 
     loss_fn = nn.CrossEntropyLoss()
-    optimizer = Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=1e-8)
+    optimizer = Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=1e-7)
     scaler = torch.cuda.amp.GradScaler()
     scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=3)
 
