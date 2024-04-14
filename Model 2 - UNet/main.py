e89465622c40c64e4d7524d4506735ee2bcd4e54
Alanzxd <112223122+Alanzxd@users.noreply.github.com>
2024-04-14 14:07:05 +0800

    Update main.py

--- Model 2 - UNet/main.py
+++ Model 2 - UNet/main.py
@@ -209,7 +209,7 @@ if __name__ == "__main__":
     # loss criterion, optimizer
 
     loss_fn = nn.CrossEntropyLoss()
-    optimizer = Adam(model.parameters(), lr=LEARNING_RATE，weight_decay=1e-8)
+    optimizer = Adam(model.parameters(), lr=LEARNING_RATE，weight_decay=1e-4)
     # 初始化调度器
     import torch.optim as optim
     from torch.optim.lr_scheduler import ReduceLROnPlateau
