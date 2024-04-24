7eb236045d44dab34bcc48f9921434a80b5fc110
Alanzxd <112223122+Alanzxd@users.noreply.github.com>
2024-04-24 10:43:20 +0800

    Update main.py

--- Model 2 - UNet/main.py
+++ Model 2 - UNet/main.py
@@ -244,7 +244,7 @@ if __name__ == "__main__":
 
     # hyperparameters
 
-    LEARNING_RATE = 0.001
+    LEARNING_RATE = 0.0005
     num_epochs = 40
     max_patience = 3
     epochs_no_improve = 0
@@ -255,7 +255,7 @@ if __name__ == "__main__":
 
     #loss_fn = nn.CrossEntropyLoss()
     loss_fn = CombinedLoss()
-    optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
+    optimizer = Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=0.0001)
     # 初始
     import torch.optim as optim
     from torch.optim.lr_scheduler import ReduceLROnPlateau
